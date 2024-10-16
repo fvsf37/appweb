bc85a954c661db756ee629f4dbf60a19aaee85e6
Francisco Salapic <fvsalapic@gmail.com>
2024-10-16 13:39:23 +0200

    formatted

--- web/public/javascripts/front.js
+++ web/public/javascripts/front.js
@@ -2,10 +2,12 @@ console.log("Hola mundo...");
 
 document.querySelector("input#lenguaje").value = navigator.userAgent;
 
-document.querySelector("a#open-offcanvas").addEventListener("click",()=>{
-    document.querySelector("aside#offcanvas").classList.add("show");
+document.querySelector("a#open-offcanvas").addEventListener("click", () => {
+  document.querySelector("aside#offcanvas").classList.add("show");
 });
 
-document.querySelector("#offcanvas button.close").addEventListener("click",()=>{
+document
+  .querySelector("#offcanvas button.close")
+  .addEventListener("click", () => {
     document.querySelector("aside#offcanvas").classList.remove("show");
-});
+  });
